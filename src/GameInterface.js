0cba7a312cab07f6421e06761581689ae0eddebe
Christopher Lemke <spcbrn1@gmail.com>
2018-10-15 10:50:15 -0600

    base finished

--- src/GameInterface.js
+++ src/GameInterface.js
@@ -4,23 +4,12 @@ import GameCanvas from './components/GameCanvas';
 import GameControls from './components/GameControls';
 
 class GameInterface extends Component {
-    constructor() {
-        super();
-        this.state = {
-            vel: 1
-        }
-    }
-
-    _updateVelocity = () => {
-        this.setState((ps, s) => ({ vel: (Math.floor(Math.random() * 6) + 1) }))
-    }
-
     render() {
         return (
             <main style={{ width: '100vw', height: '100vh', background: '#000', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <GameCanvas velocity={this.state.vel} />
-                    <GameControls updateVel={this._updateVelocity} />
+                    <GameCanvas />
+                    <GameControls />
                 </section>
             </main>
         )
